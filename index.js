429bc83ad011f2d23cf1c46f5f687cfb89c81ec2
Benjamin Bowes <bbowes999@gmail.com>
2018-01-22 18:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var Kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name){
  return kittens.push(name);
  }
- function destructivelyPrependKitten(name)
- return 
+ function destructivelyPrependKitten(name){
+ return kittens.unshift(name);
+ }
